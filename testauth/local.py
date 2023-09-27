f688d2533ee846c547604a86a1471f4fcf3d706b
Peter Pfeufer <info@ppfeufer.de>
2023-09-27 11:01:29 +0200

    [ADD] context processor to test suite

--- testauth/local.py
+++ testauth/local.py
@@ -58,6 +58,9 @@ if os.environ.get("USE_MYSQL", True) is True:
 
 # Add any additional apps to this list.
 INSTALLED_APPS.insert(0, PACKAGE)
+TEMPLATES[0]["OPTIONS"]["context_processors"].append(
+    "fireauth_templates.context_processors.fireauth_settings"
+)
 
 # By default, apps are prevented from having public views for security reasons.
 # If you want to allow specific apps to have public views,
